8c56012b00abb6ab4dd9db45bab7d8e77339f716
stestrepo <sayali4294@yahoo.co.in>
2017-11-27 14:59:12 +0530

    url log

--- demo/urls/userlogin.py
+++ demo/urls/userlogin.py
@@ -5,5 +5,8 @@ from demo.views import userlogin
 
 urlpatterns = [
     url(r'^$', views.userlogin.userlogin , name='userlogin'),
+    url(r'^ls/', views.userlogin.second , name='second'),
+    
+    url(r'^login/', views.userlogin.login , name='login'),
 
 ]
--- demo/views/userlogin.py
+++ demo/views/userlogin.py
@@ -2,11 +2,35 @@ from django.http import HttpResponse
 from demo.models import Question
 from django.template import loader
 
+
 def userlogin(request):
-    print "reached uptill"
-    return HttpResponse("Hello, world. 2ns part ppppppppppppppppppp :D.")
+    # print "reached uptill"
+    # return HttpResponse("Hello, world. 2ns part ppppppppzzzzzzzppppppppppp :D.")
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('demo/index.html')
+    # template = loader.get_template('demo/index.php')
+    detail="ls/"
+
+    print "op"
+
+    context = {
+        'latest_question_list': latest_question_list,
+        'detail':detail,
+    }
+    return HttpResponse(template.render(context, request))
+
 
     
 
+def second(request):
+    return HttpResponse("Hello, world. 2ns part.")
 
+
+
+def login(request):
+    template = loader.get_template('demo/login_page.html')
+    context = {
+	}
+    return HttpResponse(template.render(context, request))
 
